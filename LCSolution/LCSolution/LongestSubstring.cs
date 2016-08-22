ebc8c805ddebdfc487f4bf1e3cdc48410d43c1a9
Xiong, Hui P <hui.p.xiong@intel.com>
2016-08-22 15:38:03 -0700

    add solution longest substring

---
+++ LCSolution/LCSolution/LongestSubstring.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCSolution
+{
+    public class LongestSubstring
+    {
+        // Longest Substring Without Repeating Characters
+        // Given a string, find the length of the longest substring without repeating characters.
+        //Examples:
+        //Given "abcabcbb", the answer is "abc", which the length is 3.
+        //Given "bbbbb", the answer is "b", with the length of 1.
+        //Given "pwwkew", the answer is "wke", with the length of 3. 
+        //       Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0) return 0;
+
+            int n = s.Length;
+            string maxString = string.Empty;
+            string currString = string.Empty;
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            int maxlen = 0;
+            int j = 0;
+            int prevId = -1; // track last repeating pos
+            map.Add(s[0], 0);
+            int currLen = 1;
+
+            for (j = 1; j < n; j++)
+            {
+                prevId = map.ContainsKey(s[j]) ? map[s[j]] : -1;
+                if (prevId == -1 || prevId < j - currLen) // not repeating or outside of current range
+                {
+                    currLen++;
+                    currString += s[j];
+                    maxlen = maxlen > currLen ? maxlen : currLen;
+                    maxString = maxlen > currLen ? maxString : currString;
+                }
+                else
+                {
+                    currLen = j - prevId;
+                    currString = s.Substring(prevId, j - prevId);
+                }
+
+                if (map.ContainsKey(s[j]))
+                {
+                    map[s[j]] = j;
+                }
+                else
+                {
+                    map.Add(s[j], j);
+                }
+            }
+
+            //make sure nothing missed
+            maxlen = maxlen > currLen ? maxlen : currLen;
+            maxString = maxlen > currLen ? maxString : currString;
+
+            return maxlen;
+        }
+    }
+}
---
+++ LCSolution/LCSolutionTests/LongestSubstringTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LCSolution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LCSolution.Tests
+{
+    [TestClass()]
+    public class LongestSubstringTests
+    {
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest()
+        {
+            string input = string.Empty;
+            int expected = -1;
+            int actual = -1;
+            LongestSubstring sl = new LongestSubstring();
+
+            input = "hsenbppkqtpddbuotbbqcwivrfxjujjddntgeiq";
+            expected = 11;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "abbba";
+            expected = 2;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "ckilbkd";
+            expected = 5;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "abcabcbb";
+            expected = 3;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "aaabcccdeeefffhgdg";
+            expected = 4;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "aba";
+            expected = 2;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "abcdefg";
+            expected = 7;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "abcdabcd";
+            expected = 4;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+            expected = 94;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "Tempor lectus mauris integer eu, ut? Sagittis cursus. Augue proin, magna! Augue, sed urna scelerisque? Et! Proin et, lundium diam ut natoque augue porta elementum phasellus augue ac tempor nec rhoncus et scelerisque platea et nisi mattis vut pellentesque mauris, diam scelerisque, ut dignissim, amet? Vel facilisis elementum lacus eros et et tristique nisi dapibus montes massa arcu in scelerisque, eu natoque dis? Eros nascetur, ridiculus, nisi cum mauris turpis ac pid, porta purus, quis, nunc a vel! Vel adipiscing ultricies sociis vel! Sit, vel montes et quis. Lacus dis, habitasse in in montes hac turpis! Mattis sagittis? Scelerisque ut placerat lectus urna integer porttitor. Diam ultricies sociis. Sed est integer proin! Adipiscing non! Ut dis? Nunc augue porta odio.Dis mattis tincidunt ut, aliquet porta facilisis eros turpis. Mus cras scelerisque pellentesque turpis et auctor urna etiam habitasse? Tortor pellentesque ac, urna mauris, odio tristique nisi elit vut, cursus cum. Pid nunc elementum porta, nunc augue sociis quis scelerisque sed, augue penatibus dignissim ridiculus in rhoncus, urna urna parturient. Nec, adipiscing dignissim purus, augue. Sed lacus ultricies odio, non dictumst! Magna tincidunt, sit turpis eros vut ultrices enim, sit dignissim. Dictumst tortor tincidunt, augue natoque vut velit eros! Lundium eu. Est augue. Etiam nisi tincidunt ultrices, nascetur dapibus, porttitor nisi porta augue nunc cursus? Sociis proin urna sit, vel tortor? Turpis in, habitasse nascetur vel phasellus, vut rhoncus aliquam velit risus natoque. Tempor porttitor, sit magna, sed, enim.";
+            expected = 13;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+            input = "tempor lectus mauris integer eu, ut? sagit";
+            expected = 11;
+            actual = sl.LengthOfLongestSubstring(input);
+            Assert.AreEqual(expected, actual, input);
+
+        }
+    }
+}
